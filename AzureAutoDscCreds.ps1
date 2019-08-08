86cf2c0c7f4b7d00f2ef05d7e31507088932de29
Aaron Hodges <53194734+aaronhodges1126@users.noreply.github.com>
2019-08-08 14:06:15 -0500

    Create AzureAutoDscCreds.ps1

---
+++ AzureAutoDscCreds.ps1
@@ -0,0 +1,15 @@
+Configuration CredentialSample
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    $Cred = Get-AutomationPSCredential 'SomeCredentialAsset'
+
+    Node $AllNodes.NodeName
+    {
+        File ExampleFile
+        {
+            SourcePath      = '\\Server\share\path\file.ext'
+            DestinationPath = 'C:\destinationPath'
+            Credential      = $Cred
+        }
+    }
+}
